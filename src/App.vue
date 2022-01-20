f75be5ce5a6d5d02ec2a5ac40396445d24567b70
epatmalnieks <epatmalnieks04@yahoo.com>
2022-01-19 21:05:10 -0500

    show table when clicking nav

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <v-app>
-    <v-tabs v-model="selectedTeam" background-color="primary" dark>
-      <v-tab v-for="team in teams" :key="team.name">
-        {{ team.name }}
-      </v-tab>
-    </v-tabs>
-    <v-tabs-items v-model="selectedTeam">
-      <v-tab-item v-for="team in teams" :key="team.name">
-        <v-card flat>
-          <v-card-text>{{ team.roster }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-card tile="false">
+      <v-tabs v-model="selectedTeam" background-color="primary" dark>
+        <v-tab v-for="team in teams" :key="team.name">
+          {{ team.name }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="selectedTeam">
+        <v-tab-item v-for="team in teams" :key="team.name">
+          <v-simple-table dense height="500px" fixed-header>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    Position
+                  </th>
+                  <th class="text-left">
+                    Player
+                  </th>
+                  <th class="text-left">
+                    Salary
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="player in team.roster" :key="player.position">
+                  <td>{{ player.position }}</td>
+                  <td>{{ player.name }}</td>
+                  <td>{{ player.salary }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </v-app>
 </template>
 
@@ -28,7 +48,116 @@ export default {
       tab: null,
       teams: [
         { name: 'Houston Colt .45s', roster: ' team 1 roster' },
-        { name: 'Brooklyn Dodgers', roster: ' name 1 roster' },
+        {
+          name: 'Brooklyn Dodgers',
+          roster: [
+            {
+              name: 'Salvador Perez',
+              position: 'C',
+              salary: 45,
+            },
+            {
+              name: 'Freddie Freeman',
+              position: '1B',
+              salary: 75,
+            },
+            {
+              name: 'Ozzie Albies',
+              position: '2B',
+              salary: 40,
+            },
+            {
+              name: 'Rafael Devers',
+              position: '3B',
+              salary: 46,
+            },
+            {
+              name: 'Javier Baez',
+              position: 'SS',
+              salary: 31,
+            },
+            {
+              name: 'Starling Marte',
+              position: 'OF1',
+              salary: 35,
+            },
+            {
+              name: 'George Springer',
+              position: 'OF2',
+              salary: 32,
+            },
+            {
+              name: 'Kyle Tucker',
+              position: 'OF3',
+              salary: 5,
+            },
+            {
+              name: 'Eddie Rosario',
+              position: 'Util',
+              salary: 23,
+            },
+            {
+              name: 'Aaron Nola',
+              position: 'SP1',
+              salary: 165,
+            },
+            {
+              name: 'Jack Flaherty',
+              position: 'SP2',
+              salary: 165,
+            },
+            {
+              name: 'Raisel Iglesias',
+              position: 'RP1',
+              salary: 93,
+            },
+            {
+              name: 'Kirby Yates',
+              position: 'RP2',
+              salary: 57,
+            },
+            {
+              name: 'Brandon Woodruff',
+              position: 'P1',
+              salary: 165,
+            },
+            {
+              name: 'Zac Gallen',
+              position: 'P2',
+              salary: 190,
+            },
+            {
+              name: 'Ryan Pressly',
+              position: 'P3',
+              salary: 69,
+            },
+            {
+              name: 'Tyler Mahle',
+              position: 'B1',
+              salary: 54,
+            },
+            {
+              name: 'Brady Singer',
+              position: 'B2',
+              salary: 14,
+            },
+            {
+              name: 'Elieser Hernandez',
+              position: 'B3',
+              salary: 30,
+            },
+            {
+              name: 'Adley Rutschman',
+              position: 'B4',
+              salary: 0,
+            },
+            {
+              name: 'Joakim Soria',
+              position: 'B5',
+              salary: 3,
+            },
+          ],
+        },
         { name: 'Detroit Stars', roster: ' name 1 roster' },
         { name: 'Zulu Cannibal Giants', roster: ' name 2 roster' },
         { name: 'Boston Braves', roster: ' name 3 roster' },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,141 +1,14 @@
 <template>
-  <v-simple-table dense height="500px" fixed-header>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">
-              Position
-            </th>
-            <th class="text-left">
-              Player
-            </th>
-            <th class="text-left">
-              Salary
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="player in players" :key="player.player">
-            <td>{{ player.position }}</td>
-            <td>{{ player.name }}</td>
-            <td>{{ player.salary }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+<div>
+test
+</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      players: [
-        {
-          name: 'Salvador Perez',
-          position: 'C',
-          salary: 45,
-        },
-        {
-          name: 'Freddie Freeman',
-          position: '1B',
-          salary: 75,
-        },
-        {
-          name: 'Ozzie Albies',
-          position: '2B',
-          salary: 40,
-        },
-        {
-          name: 'Rafael Devers',
-          position: '3B',
-          salary: 46,
-        },
-        {
-          name: 'Javier Baez',
-          position: 'SS',
-          salary: 31,
-        },
-        {
-          name: 'Starling Marte',
-          position: 'OF1',
-          salary: 35,
-        },
-        {
-          name: 'George Springer',
-          position: 'OF2',
-          salary: 32,
-        },
-        {
-          name: 'Kyle Tucker',
-          position: 'OF3',
-          salary: 5,
-        },
-        {
-          name: 'Eddie Rosario',
-          position: 'Util',
-          salary: 23,
-        },
-        {
-          name: 'Aaron Nola',
-          position: 'SP1',
-          salary: 165,
-        },
-        {
-          name: 'Jack Flaherty',
-          position: 'SP2',
-          salary: 165,
-        },
-        {
-          name: 'Raisel Iglesias',
-          position: 'RP1',
-          salary: 93,
-        },
-        {
-          name: 'Kirby Yates',
-          position: 'RP2',
-          salary: 57,
-        },
-        {
-          name: 'Brandon Woodruff',
-          position: 'P1',
-          salary: 165,
-        },
-        {
-          name: 'Zac Gallen',
-          position: 'P2',
-          salary: 190,
-        },
-        {
-          name: 'Ryan Pressly',
-          position: 'P3',
-          salary: 69,
-        },
-        {
-          name: 'Tyler Mahle',
-          position: 'B1',
-          salary: 54,
-        },
-        {
-          name: 'Brady Singer',
-          position: 'B2',
-          salary: 14,
-        },
-        {
-          name: 'Elieser Hernandez',
-          position: 'B3',
-          salary: 30,
-        },
-        {
-          name: 'Adley Rutschman',
-          position: 'B4',
-          salary: 0,
-        },
-        {
-          name: 'Joakim Soria',
-          position: 'B5',
-          salary: 3,
-        },
-      ],
+
     };
   },
   name: 'Home',
